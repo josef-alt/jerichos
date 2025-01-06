43c1ae6c5ce9bbb4e8ed24cf382bbdb822859999
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2025-01-05 21:35:38 -0600

    loaded favorites into Favorites tab

--- app/Favorites.jsx
+++ app/Favorites.jsx
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native'
+import { Text } from 'react-native'
 import React from 'react'
+import { getFavorites } from './data/db'
 
 export default function Favorites() {
+    // track whether or not database is ready
+    const [favorites, setFavorites] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        try {
+            const data = getFavorites();
+            setFavorites(data);
+            setLoading(false);
+        } catch(err) {
+            console.log('error', err);
+        }
+    }, []);
+
+    if(loading) {
+        return (
+            <SafeAreaView style={styles.mainContainer}>
+                <Text>Loading...</Text>
+                <StatusBar style="auto" />
+            </SafeAreaView>
+        );
+    }
+
+    if(favorites === undefined || favorites.length == 0) {
+        return (
+            <SafeAreaView style={styles.mainContainer}>
+                <Text>You need to add some favorites first!</Text>
+                <StatusBar style="auto" />
+            </SafeAreaView>
+        );
+    }
+
     return (
-        <View>
-            <Text>faves</Text>
-        </View>
-    )
+        <SafeAreaView style={styles.mainContainer}>
+            <FlatList
+                contentContainerStyle={{gap:5}}
+                data={favorites}
+                keyExtractor={(item) => item.recipeName}
+                renderItem={ ({item}) => <RecipeCard item={item} /> }
+            />
+            <StatusBar style="auto" />
+        </SafeAreaView>
+    );
 }
