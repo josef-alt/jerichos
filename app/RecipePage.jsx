0a72daa06eab43ca33373f4b96eaa0e239959152
trawnlegacy <30385688+trawnlegacy@users.noreply.github.com>
2025-01-14 21:51:01 -0600

    added ingredients and steps to recipe page as raw text

--- app/RecipePage.jsx
+++ app/RecipePage.jsx
@@ -1,14 +1,35 @@
 import { useLocalSearchParams } from 'expo-router/build/hooks';
-import { Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { getRecipe } from './data/db';
 
 export default function RecipePage() {
     const { item } = useLocalSearchParams();
-    const recipe = JSON.parse(item);
+    let recipe = JSON.parse(item);
+    recipe = getRecipe(recipe.recipeId);
 
     return (
+        // photos? editing? folders?
         <SafeAreaView style={styles.mainContainer}>
-            <Text>{recipe.recipeName}</Text>
+            <View style={styles.headerContainer}>
+                <Text>{recipe.recipeName}</Text>
+                <Text>{recipe.categoryName}</Text>
+            </View>
+
+            <View style={styles.bodyContainer}>
+                <View style={styles.ingredientsList}>
+                    <Text>ingredients</Text>{
+                    recipe.ingredients.map((ing, idx) => (
+                        <Text>{ing.name}</Text>
+                    ))}
+                </View>
+                <View style={styles.stepsList}>
+                    <Text>instructions</Text>{
+                    recipe.steps.map((step, idx) => (
+                        <Text>{step.description}</Text>
+                    ))}
+                </View>
+            </View>
         </SafeAreaView>
     )
 }
